38aa290535c3d599294fcba8095020ca73c8f73a
aeesh <adamaisha352@gmail.com>
2022-04-07 13:40:19 +0100

    redux initialized

---
+++ src/reduxContent/addWallet/reducers.ts
@@ -0,0 +1,20 @@
+import { GET_DATA } from './types';
+
+const initState = {
+    data: {}
+};
+
+const getDataReducer = (state = initState, action:any) => {
+    switch (action.type) {
+    case GET_DATA:
+        return {
+            ...state,
+            isFetching: false,
+            data: action.payload
+        };
+    default:
+        return state;
+    }
+};
+
+export default getDataReducer;
